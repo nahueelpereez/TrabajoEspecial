45ead09d03967d3d4981ae5acd7fb1a04c921d35
LautaroFrijon <42587900+LautaroFrijon@users.noreply.github.com>
2019-11-26 09:43:35 -0300

    Update route-api.php

--- route-api.php
+++ route-api.php
@@ -6,12 +6,9 @@ $r = new apiRouter();
 //tabla de ruteo
 $r->addRoute("comentarios", "GET", "comentariosController", "obtenerComentarioss");
 
-$r->addRoutew("comentario", "GET", "comentariosController", "obtenerComentario");
+$r->addRoute("comentarios/:ID", "DELETE", "comentariosController", "borrar");
 
-$r->addRoute("borrarComentario", "DELETE", "comentariosController", "borrar");
-
-$r->addRoute("formAgregar", "GET", "comentariosController", "mostrarForm");
-$r->addRoute("agregarComentario", "POST", "comentariosController", "agregarComentario");
+$r->addRoute("comentarios", "POST", "comentariosController", "agregarComentario");
 
 
 
