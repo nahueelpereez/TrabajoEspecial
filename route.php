d67d0fc7587a30fd725735cd9c1ac1d1de5ad621
nahueelpereez <57244530+nahueelpereez@users.noreply.github.com>
2019-12-03 21:48:53 -0300

    Update route.php

--- route.php
+++ route.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('Controllers/equipoController.php');
+    require_once('Controllers/EquiposController.php');
     require_once('Controllers/noticiaController.php');
     require_once('Controllers/loginController.php');
     require_once('router.php');
@@ -18,19 +18,20 @@
     $r->addRoute("equipo/:ID", "GET", "EquiposController", "obtenerEquipo");
     $r->addRoute("noticias/:ID", "GET", "NoticiasController", "obtenerNoticia");
 
-    $r->addRoute("eliminar/:ID", "GET", "NoticiasController", "eliminarNoticia");
-    $r->addRoute("equipos/:ID", "GET", "EquiposController", "eliminarEquipo");
+    $r->addRoute("eliminarNoticia/:ID", "GET", "NoticiasController", "eliminarNoticia");
+    $r->addRoute("eliminarEquipo/:ID", "GET", "EquiposController", "eliminarEquipo");
 
-    $r->addRoute("editarEquipo", "GET", "EquiposController", "editarEquipo");
-
+    
     $r->addRoute("equipos", "GET", "EquiposController", "ShowEquipos");
-    $r->addRoute("editar/:ID", "POST", "NoticiasController", "editarNoticia");
-
+    $r->addRoute("editarNoticia/:ID", "POST", "NoticiasController", "editarNoticia");
+    $r->addRoute("editarEquipo/:ID", "POST", "EquiposController", "editarEquipo");
+    
     $r->addRoute("deleteUsuario/:ID", "GET", "LoginController", "borrarusuario");
 
     
     $r->addRoute("mostrarAdministradores", "GET", "LoginController", "showAdministradores");
     $r->addRoute("mostrarFormEquipo", "GET", "EquiposController", "showForm");
+    $r->addRoute("formEditarEquipo/:ID", "GET", "EquiposController", "showFormEditar");
     $r->addRoute("mostrarFormNoticia", "GET", "NoticiasController", "showForm");
     $r->addRoute("mostrarFormRegistro", "GET", "LoginController", "showRegistro");
     $r->addRoute("nuevaNoticia", "POST", "NoticiasController", "agregarNoticia");
