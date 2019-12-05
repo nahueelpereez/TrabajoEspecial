67d696e42b1b0bd94de98e52c04744b700574ad2
nahueelpereez <57244530+nahueelpereez@users.noreply.github.com>
2019-12-04 22:58:59 -0300

    Update noticiaController.php

--- Controllers/noticiaController.php
+++ Controllers/noticiaController.php
@@ -3,6 +3,7 @@
 require_once('Models/noticiaModel.php');
 require_once('Views/noticiaView.php');
 require_once('AyudaLogin/autenticacion.php');
+require_once('Models/equiposModel.php');
 
 class NoticiasController{
 
@@ -12,6 +13,7 @@ class NoticiasController{
     public function __construct(){
         $this->view = new NoticiaView();
         $this->model = new NoticiaModel();
+        $this->modelE = new EquiposModel();
     }
 
 
@@ -33,12 +35,11 @@ class NoticiasController{
             $this->view->mostrarError('La noticia no existe');
     }
 
-     public function agregarNoticia(){
+    public function agregarNoticia() {
         $autenticador = new Autenticacion();
         $autenticador->checkLoggedIn();
-        
-        // agarra el file
-        if ($_FILES['imagen']['name']) {
+         // agarra el file
+         if ($_FILES['imagen']['name']) {
             if ($_FILES['imagen']['type'] == "image/jpeg" || $_FILES['imagen']['type'] == "image/jpg" || $_FILES['imagen']['type'] == "image/png") {
                 
                 $this->model->guardar($_POST['titulo'], $_POST['fecha'], $_POST['contenido'], $_POST['equipo'], $_FILES['imagen']);
@@ -51,7 +52,7 @@ class NoticiasController{
         else {
             $this->model->guardar($_POST['titulo'], $_POST['fecha'], $_POST['contenido'], $_POST['equipo']);  
         }
-        header("Location: " . basehref);
+        header("Location: " . basehref . noticias);
     }
 
     public function eliminarNoticia($params = null) {
@@ -61,33 +62,38 @@ class NoticiasController{
         
         $idNoticia = $params[':ID'];
         $this->model->borrar($idNoticia);
-        header("Location: " . basehref);
+        header("Location: " . basehref . noticias);
     }
 
     public function showForm(){
         
-        $this->view->showForm();
+        $noticias = $this->modelE->getEquipos();
+        $this->view->showForm($noticias);
 
     }
 
    public function editarNoticia(){
-        $id_noticia = $_POST['id_noticia'];
+    if(isset($_POST['titulo']) && (isset($_POST['fecha'])) && (isset($_POST['contenido'])) && (isset($_POST['equipo'])) && (isset($_POST['id_notica']))){
         $titulo = $_POST['titulo'];
         $fecha = $_POST['fecha'];
-        $descripcion = $_POST['descripcion'];
-
-        if(!empty($titulo) && !empty($fecha) && !empty($descripcion)){
-            $this->model->editar($titulo, $fecha, $descripcion);
-            header("Location: " . basehref . noticias);
-        }
+        $contenido = $_POST['contenido'];
+        $idEquipo = $_POST['equipo'];
+        $id_noticia = $_POST['id_notica'];
+        $imagen = $_FILES['imagen'];
+        $this->model->editarNoticia($titulo, $fecha, $contenido, $idEquipo, $imagen, $id_noticia);
+        header("Location: " . basehref . noticias);
     }
-    
-    public function showFormEditar($param = null){
-        $idNoticia = $param[':ID'];
-        $noticia = $this->model->getNoticia($idNoticia);
+    else {
+        $this->view->mostrarError("Faltan datos obligatorios");
+    }
+}
+
+    public function showFormEditar($params = null){
         
-        $this->view->showFormEditar($noticia);
-    }
-
-
+        $idNoticia = $params[':ID'];
+        $noticia = $this->model->getNoticia($idNoticia);
+        $noticias = $this->modelE->getEquipos();
+        $this->view->showFormEditar($noticia, $noticias);   
+    }  
+    
 }
