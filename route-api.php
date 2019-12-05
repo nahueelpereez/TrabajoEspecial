9da3a2aaeabba8b893fb9343d6e2a0a2b01f399e
LautaroFrijon <42587900+LautaroFrijon@users.noreply.github.com>
2019-12-05 09:25:41 -0300

    Update route-api.php

--- route-api.php
+++ route-api.php
@@ -1,17 +1,29 @@
 <?php
+require_once('router.php');
+require_once('api/comentariosApiController.php');
+
+define("basehref", 'http://'.$_SERVER["SERVER_NAME"].':'.$_SERVER["SERVER_PORT"].dirname($_SERVER["PHP_SELF"]).'/');
+
+// recurso solicitado
+$resource = $_GET["resource"];
+
+// método utilizado
+$method = $_SERVER["REQUEST_METHOD"];
+
 
 //creo el router
-$r = new apiRouter();
+$r = new Router();
 
 //tabla de ruteo
-$r->addRoute("comentarios", "GET", "comentariosController", "obtenerComentarioss");
+$r->addRoute("comentarios", "GET", "comentariosApiController", "obtenerComentarios");
 
-$r->addRoute("comentarios/:ID", "DELETE", "comentariosController", "borrar");
+$r->addRoute("comentarios/:ID", "GET", "comentariosApiController", "obtenerComentario");
 
-$r->addRoute("comentarios", "POST", "comentariosController", "agregarComentario");
+$r->addRoute("comentarios/:ID", "DELETE", "comentariosApiController", "eliminar");
 
+$r->addRoute("comentarios", "POST", "comentariosApiController", "agregarComentario");
 
 
 
 // rutea
-$router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
+$r->route($resource, $method);
